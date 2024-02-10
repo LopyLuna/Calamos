e5d68d29129138e895cecc9c05b4ac6d375dc442
KhanhTypo <68932626+KhanhPham05@users.noreply.github.com>
2024-02-10 15:23:22 +0700

    add item data generator for handheld models as templates

--- src/main/java/uwu/lopyluna/calamos/datagen/ModItemModelProvider.java
+++ src/main/java/uwu/lopyluna/calamos/datagen/ModItemModelProvider.java
@@ -1,14 +1,18 @@
 package uwu.lopyluna.calamos.datagen;
 
+import net.minecraft.core.Holder;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import uwu.lopyluna.calamos.elements.ModItems;
+import uwu.lopyluna.calamos.utilities.ModUtils;
 
 import java.util.function.Supplier;
 
-public class ModItemModelProvider extends ItemModelProvider {
+class ModItemModelProvider extends ItemModelProvider {
     public ModItemModelProvider(PackOutput output, String modid, ExistingFileHelper existingFileHelper) {
         super(output, modid, existingFileHelper);
     }
@@ -31,5 +35,23 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     private void basicItem(Supplier<? extends Item> item) {
         super.basicItem(item.get());
+    }
+
+    private void handheld32(DeferredHolder<Item, ? extends Item> item) {
+        handheld(item, 32);
+    }
+
+    private void handheld48(DeferredHolder<Item, ? extends Item> item) {
+        handheld(item, 48);
+    }
+
+    private void handheld64(DeferredHolder<Item, ? extends Item> item) {
+        handheld(item, 64);
+    }
+
+    private void handheld(DeferredHolder<Item, ? extends Item> item, int x) {
+        ResourceLocation name = item.getKey().location().withPrefix("item/");
+        super.withExistingParent(name.toString(), ModUtils.location("item/templates/handheld%sx".formatted(x)))
+                .texture("layer0", name);
     }
 }
