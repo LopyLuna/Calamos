6b02e496f09e956012c710ab38439d005f485bb4
xuwu / luna <loganalexzand@gmail.com>
2024-03-19 10:28:38 -0800

    TNT & Dynamite

---
+++ src/main/java/uwu/lopyluna/calamos/elements/entity/dynamite/DynamiteModel.java
@@ -0,0 +1,47 @@
+package uwu.lopyluna.calamos.elements.entity.dynamite;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+
+import static uwu.lopyluna.calamos.CalamosMod.MODID;
+
+/**
+ * Made with Blockbench 4.9.4
+ * Exported for Minecraft version 1.17 or later with Mojang mappings
+ * Paste this class into your mod and generate all required imports
+ * @author lopyluna
+ */
+public class DynamiteModel<T extends Entity> extends EntityModel<T> {
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(MODID, "dynamite"), "main");
+	private final ModelPart dynamite;
+
+	public DynamiteModel(ModelPart root) {
+		this.dynamite = root.getChild("dynamite");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition dynamite = partdefinition.addOrReplaceChild("dynamite", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -4.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 20.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+	@Override
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+		dynamite.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+}
